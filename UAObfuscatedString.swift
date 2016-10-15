250f276ab4551c04ead0dd2a8c96060c44699f3f
Adar Porat <adar.porat@gmail.com>
2016-10-15 12:34:54 -0400

    prefix all functions with underscore

--- UAObfuscatedString.swift
+++ UAObfuscatedString.swift
@@ -7,62 +7,62 @@
 
 // MARK: - a-z -
 public extension String {
-    var a : String { get { return self + "a" } }
-    var b : String { get { return self + "b" } }
-    var c : String { get { return self + "c" } }
-    var d : String { get { return self + "d" } }
-    var e : String { get { return self + "e" } }
-    var f : String { get { return self + "f" } }
-    var g : String { get { return self + "g" } }
-    var h : String { get { return self + "h" } }
-    var i : String { get { return self + "i" } }
-    var j : String { get { return self + "j" } }
-    var k : String { get { return self + "k" } }
-    var l : String { get { return self + "l" } }
-    var m : String { get { return self + "m" } }
-    var n : String { get { return self + "n" } }
-    var o : String { get { return self + "o" } }
-    var p : String { get { return self + "p" } }
-    var q : String { get { return self + "q" } }
-    var r : String { get { return self + "r" } }
-    var s : String { get { return self + "s" } }
-    var t : String { get { return self + "t" } }
-    var u : String { get { return self + "u" } }
-    var v : String { get { return self + "v" } }
-    var w : String { get { return self + "w" } }
-    var x : String { get { return self + "x" } }
-    var y : String { get { return self + "y" } }
-    var z : String { get { return self + "z" } }
+    var _a : String { get { return self + "a" } }
+    var _b : String { get { return self + "b" } }
+    var _c : String { get { return self + "c" } }
+    var _d : String { get { return self + "d" } }
+    var _e : String { get { return self + "e" } }
+    var _f : String { get { return self + "f" } }
+    var _g : String { get { return self + "g" } }
+    var _h : String { get { return self + "h" } }
+    var _i : String { get { return self + "i" } }
+    var _j : String { get { return self + "j" } }
+    var _k : String { get { return self + "k" } }
+    var _l : String { get { return self + "l" } }
+    var _m : String { get { return self + "m" } }
+    var _n : String { get { return self + "n" } }
+    var _o : String { get { return self + "o" } }
+    var _p : String { get { return self + "p" } }
+    var _q : String { get { return self + "q" } }
+    var _r : String { get { return self + "r" } }
+    var _s : String { get { return self + "s" } }
+    var _t : String { get { return self + "t" } }
+    var _u : String { get { return self + "u" } }
+    var _v : String { get { return self + "v" } }
+    var _w : String { get { return self + "w" } }
+    var _x : String { get { return self + "x" } }
+    var _y : String { get { return self + "y" } }
+    var _z : String { get { return self + "z" } }
 }
 
 // MARK: - A-Z -
 public extension String {
-    var A : String { get { return self + "A" } }
-    var B : String { get { return self + "B" } }
-    var C : String { get { return self + "C" } }
-    var D : String { get { return self + "D" } }
-    var E : String { get { return self + "E" } }
-    var F : String { get { return self + "F" } }
-    var G : String { get { return self + "G" } }
-    var H : String { get { return self + "H" } }
-    var I : String { get { return self + "I" } }
-    var J : String { get { return self + "J" } }
-    var K : String { get { return self + "K" } }
-    var L : String { get { return self + "L" } }
-    var M : String { get { return self + "M" } }
-    var N : String { get { return self + "N" } }
-    var O : String { get { return self + "O" } }
-    var P : String { get { return self + "P" } }
-    var Q : String { get { return self + "Q" } }
-    var R : String { get { return self + "R" } }
-    var S : String { get { return self + "S" } }
-    var T : String { get { return self + "T" } }
-    var U : String { get { return self + "U" } }
-    var V : String { get { return self + "V" } }
-    var W : String { get { return self + "W" } }
-    var X : String { get { return self + "X" } }
-    var Y : String { get { return self + "Y" } }
-    var Z : String { get { return self + "Z" } }
+    var _A : String { get { return self + "A" } }
+    var _B : String { get { return self + "B" } }
+    var _C : String { get { return self + "C" } }
+    var _D : String { get { return self + "D" } }
+    var _E : String { get { return self + "E" } }
+    var _F : String { get { return self + "F" } }
+    var _G : String { get { return self + "G" } }
+    var _H : String { get { return self + "H" } }
+    var _I : String { get { return self + "I" } }
+    var _J : String { get { return self + "J" } }
+    var _K : String { get { return self + "K" } }
+    var _L : String { get { return self + "L" } }
+    var _M : String { get { return self + "M" } }
+    var _N : String { get { return self + "N" } }
+    var _O : String { get { return self + "O" } }
+    var _P : String { get { return self + "P" } }
+    var _Q : String { get { return self + "Q" } }
+    var _R : String { get { return self + "R" } }
+    var _S : String { get { return self + "S" } }
+    var _T : String { get { return self + "T" } }
+    var _U : String { get { return self + "U" } }
+    var _V : String { get { return self + "V" } }
+    var _W : String { get { return self + "W" } }
+    var _X : String { get { return self + "X" } }
+    var _Y : String { get { return self + "Y" } }
+    var _Z : String { get { return self + "Z" } }
 }
 
 // MARK: - Numbers -
@@ -82,41 +82,41 @@ public extension String {
 
 // MARK: - Punctuation -
 public extension String {
-    var space         : String { get { return self + " " } }
-    var point         : String { get { return self + "." } }
-    var dash          : String { get { return self + "-" } }
-    var comma         : String { get { return self + "," } }
-    var semicolon     : String { get { return self + ";" } }
-    var colon         : String { get { return self + ":" } }
-    var apostrophe    : String { get { return self + "'" } }
-    var quotation     : String { get { return self + "\"" } }
-    var plus          : String { get { return self + "+" } }
-    var equals        : String { get { return self + "=" } }
-    var paren_left    : String { get { return self + "(" } }
-    var paren_right   : String { get { return self + ")" } }
-    var asterisk      : String { get { return self + "*" } }
-    var ampersand     : String { get { return self + "&" } }
-    var caret         : String { get { return self + "^" } }
-    var percent       : String { get { return self + "%" } }
-    var $             : String { get { return self + "$" } }
-    var pound         : String { get { return self + "#" } }
-    var at            : String { get { return self + "@" } }
-    var exclamation   : String { get { return self + "!" } }
-    var question_mark : String { get { return self + "?" } }
-    var back_slash    : String { get { return self + "\\" } }
-    var forward_slash : String { get { return self + "/" } }
-    var curly_left    : String { get { return self + "{" } }
-    var curly_right   : String { get { return self + "}" } }
-    var bracket_left  : String { get { return self + "[" } }
-    var bracket_right : String { get { return self + "]" } }
-    var bar           : String { get { return self + "|" } }
-    var less_than     : String { get { return self + "<" } }
-    var greater_than  : String { get { return self + ">" } }
-    var underscore    : String { get { return self + "_" } }
+    var _space         : String { get { return self + " " } }
+    var _point         : String { get { return self + "." } }
+    var _dash          : String { get { return self + "-" } }
+    var _comma         : String { get { return self + "," } }
+    var _semicolon     : String { get { return self + ";" } }
+    var _colon         : String { get { return self + ":" } }
+    var _apostrophe    : String { get { return self + "'" } }
+    var _quotation     : String { get { return self + "\"" } }
+    var _plus          : String { get { return self + "+" } }
+    var _equals        : String { get { return self + "=" } }
+    var _paren_left    : String { get { return self + "(" } }
+    var _paren_right   : String { get { return self + ")" } }
+    var _asterisk      : String { get { return self + "*" } }
+    var _ampersand     : String { get { return self + "&" } }
+    var _caret         : String { get { return self + "^" } }
+    var _percent       : String { get { return self + "%" } }
+    var _$             : String { get { return self + "$" } }
+    var _pound         : String { get { return self + "#" } }
+    var _at            : String { get { return self + "@" } }
+    var _exclamation   : String { get { return self + "!" } }
+    var _question_mark : String { get { return self + "?" } }
+    var _back_slash    : String { get { return self + "\\" } }
+    var _forward_slash : String { get { return self + "/" } }
+    var _curly_left    : String { get { return self + "{" } }
+    var _curly_right   : String { get { return self + "}" } }
+    var _bracket_left  : String { get { return self + "[" } }
+    var _bracket_right : String { get { return self + "]" } }
+    var _bar           : String { get { return self + "|" } }
+    var _less_than     : String { get { return self + "<" } }
+    var _greater_than  : String { get { return self + ">" } }
+    var _underscore    : String { get { return self + "_" } }
 }
 
 // MARK: - Aliases -
 public extension String {
-    var dot         : String { get { return point } }
+    var _dot         : String { get { return _point } }
 }
 
